7061c81e38915d2a2d3d0b1ddf79cb925c64b676
Subir Kumar Sao <subirkumarsao@gmail.com>
2015-09-17 01:39:02 +0530

    Spring MVC 4.2.1 integration.

---
+++ src/main/java/com/lazybuds/paperless/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.lazybuds.paperless.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+
+		model.addAttribute("message", "Spring 4 MVC Hello World");
+		return "hello";
+
+	}
+}
